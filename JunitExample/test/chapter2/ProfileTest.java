085615ecb72c22904807ea4b487bd0d1f6bb00a2
gwanjin <jkj6425@gmail.com>
2020-07-12 13:02:53 +0900

    2020/07/12 JUnit @beforeXXX

--- JunitExample/test/chapter2/ProfileTest.java
+++ JunitExample/test/chapter2/ProfileTest.java
@@ -2,42 +2,56 @@ package chapter2;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ProfileTest {
+	private Profile mProfile;
+	private BooleanQuestion mQuestion;
+	private Criteria mCriteria;
+	
+	/**
+	 * 공통적인 초기화 코드를 작성하기 위한 메소드
+	 * @BeforeEach 어노에티션을 이용하면 JUnit테스트를 실행할 때마다 먼저 실행 
+	 * @BeforeEach 는 각 테스트를 실행하기 전에 한번씩 호출
+	 * @BeforeAll 은 테스트를 실행하기 전에 한번만 호
+	 */
+	@BeforeEach
+	public void create() {
+		mProfile = new Profile("Bull Hockey");
+		mQuestion = new BooleanQuestion(1, "Got bonuses?");
+		mCriteria = new Criteria();
+	}
 
 	@Test
 	void test() {
-		Profile profile = new Profile("Bull Hockey");
-		Question question = new BooleanQuestion(1, "Got bonuses?");
-		
 		// Profile객체에 적절한 Answer객체 넣어두기
-		Answer profileAnswer = new Answer(question, Bool.FALSE);
-		profile.add(profileAnswer);
+		Answer profileAnswer = new Answer(mQuestion, Bool.FALSE);
+		mProfile.add(profileAnswer);
 		
-		Criteria criteria = new Criteria();
-		Answer answer = new Answer(question, Bool.TRUE);
+		Answer answer = new Answer(mQuestion, Bool.TRUE);
 		Criterion criterion = new Criterion(answer, Weight.MustMatch);
 		
-		criteria.add(criterion);
+		mCriteria.add(criterion);
 	}
 	
 	
 	@Test
 	public void matchAnswersFalseWhenMushMathCriteriaNotMet() { // 테스트 의도를 알수있도록 적절한 이름을 지정
-		Profile profile = new Profile("Bull Hockey");
-		Question question = new BooleanQuestion(1, "Got bonuses?");
-		Answer profileAnswer = new Answer(question, Bool.FALSE);
-		profile.add(profileAnswer);
-		Criteria criteria = new Criteria();
-		Answer answer = new Answer(question, Bool.TRUE);
-		Criterion criterion = new Criterion(answer, Weight.MustMatch);
+		mProfile.add(new Answer(mQuestion, Bool.FALSE)); //지원자의 대답
+		mCriteria.add(new Criterion(new Answer(mQuestion, Bool.TRUE), Weight.MustMatch)); // 기대하는 대답
 		
-		criteria.add(criterion);
-		
-		boolean matches = profile.matches(criteria);
+		boolean matches = mProfile.matches(mCriteria);
 		// assertFalse(matches);
 		assertTrue(matches); // expected: <true> but was: <false>
+	}
+	
+	@Test
+	public void mathchAnswersTrueForAnyDontCareCriteria() {
+		mProfile.add(new Answer(mQuestion, Bool.FALSE)); //지원자의 대답
+		mCriteria.add(new Criterion(new Answer(mQuestion, Bool.TRUE), Weight.DontCare)); // 기대하는 대답
 		
+		boolean matches = mProfile.matches(mCriteria);
+		assertTrue(matches);
 	}
 }
